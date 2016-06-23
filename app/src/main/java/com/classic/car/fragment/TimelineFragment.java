fe8e0d628a2b95e241d35c99eb9f7b7a0b32bda4
qyxxjd <pgliubin@gmail.com>
2016-06-23 08:25:46 +0800

    add timeline layout

--- app/src/main/java/com/classic/car/fragment/TimelineFragment.java
+++ app/src/main/java/com/classic/car/fragment/TimelineFragment.java
@@ -1,19 +1,21 @@
 package com.classic.car.fragment;
 
 import android.os.Bundle;
+import android.support.v4.graphics.ColorUtils;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.classic.adapter.BaseAdapterHelper;
 import com.classic.adapter.CommonRecyclerAdapter;
 import com.classic.car.R;
 import com.classic.car.consts.Consts;
 import com.classic.car.entity.ConsumerDetail;
 import com.classic.car.utils.Util;
+import com.classic.car.widget.CircleImageView;
 import com.classic.core.fragment.BaseFragment;
 import com.classic.core.utils.DateUtil;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 应用名称: CarAssistant
@@ -25,7 +27,7 @@ import java.util.List;
  */
 public class TimelineFragment extends BaseFragment {
 
-    private RecyclerView mRecyclerView;
+    @BindView(R.id.timeline_recycler_view) RecyclerView mRecyclerView;
 
     @Override public int getLayoutResId() {
         return R.layout.fragment_timeline;
@@ -33,38 +35,24 @@ public class TimelineFragment extends BaseFragment {
 
     @Override public void initView(View parentView, Bundle savedInstanceState) {
         super.initView(parentView, savedInstanceState);
-        mRecyclerView = (RecyclerView) parentView.findViewById(R.id.recycler_view);
+        ButterKnife.bind(this, parentView);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(activity));
         mRecyclerView.setAdapter(
-                new CommonRecyclerAdapter<ConsumerDetail>(activity, R.layout.item_timeline, getTempData()) {
+                new CommonRecyclerAdapter<ConsumerDetail>(activity, R.layout.item_timeline, Util.getTestData()) {
                     @Override public void onUpdate(BaseAdapterHelper helper, ConsumerDetail item, int position) {
-                        helper.setText(R.id.timeline_time,
-                                DateUtil.formatDate(DateUtil.FORMAT_DATE, item.getConsumptionTime()))
-                              .setText(R.id.timeline_content,
+                        CircleImageView civ = helper.getView(R.id.item_timeline_icon_bg);
+                        int color = getResources().getColor(Util.getColorByType(item.getType()));
+                        civ.setFillColor(color);
+                        civ.setBorderColor(ColorUtils.setAlphaComponent(color, 100));
+                        helper.setText(R.id.item_timeline_time,
+                                DateUtil.formatDate(DateUtil.FORMAT_DATE, item.getConsumptionTime()) + "\n" +
+                                        DateUtil.formatDate(DateUtil.FORMAT_TIME, item.getConsumptionTime())
+                                )
+                              .setImageResource(R.id.item_timeline_icon, Util.getIconByType(item.getType()))
+                              .setText(R.id.item_timeline_content,
                                       Consts.TYPE_MENUS[item.getType()] + "\n" + Util.formatMoney(item.getMoney()))
-                              .setTextColorRes(R.id.timeline_content, Util.getColorByType(item.getType()));
-
-                        //CircleImageView civ = helper.getView(R.id.timeline_icon);
-                        //civ.setImageResource(Util.getIconByType(item.getType()));
-                        //civ.setBackgroundColor(Util.getColorByType(item.getType()));
+                              .setTextColorRes(R.id.item_timeline_content, Util.getColorByType(item.getType()));
                     }
                 });
-    }
-
-    private List<ConsumerDetail> getTempData(){
-        List<ConsumerDetail> data = new ArrayList<>();
-        for (int i = 0; i<8; i++){
-            ConsumerDetail item = new ConsumerDetail(System.currentTimeMillis(),
-                    666, i);
-            if(i == 1 || i==7){
-                item.setNotes("致力于提供世界一流的 Android 和 iOS 应用开发服务, 因一些动画很棒的开源库为大家所熟知。");
-            }
-            if (i == 3 || i == 5) {
-                item.setNotes(
-                        "灵感UI设计网是一个致力于设计师UI设计作品分享的网站，提供很多有才华的设计师们的优秀作品，帮助设计师们获得UI方面的设计灵感，提供丰富的设计参考案例，同时还提供收费版的在线设计工具。");
-            }
-            data.add(item);
-        }
-        return data;
     }
 }
